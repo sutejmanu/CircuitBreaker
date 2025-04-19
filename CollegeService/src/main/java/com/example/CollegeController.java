ae5d5e898b863d3a945562e8785af421a1cc86da
sutejmanu <ravikumarlakkakula2012@gmail.com>
2025-04-19 12:51:04 +0530

    circuit breaker demo

---
+++ CollegeService/src/main/java/com/example/CollegeController.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CollegeController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private static final String COLLEGE_SERVICE = "collegeService";
+
+    int i = 0;
+
+    @GetMapping("/college/student")
+    @Retry(name = COLLEGE_SERVICE, fallbackMethod = "getDefaultStudentName")
+    public String getStudentName(){
+        i = i + 1;
+        System.out.println("Attempt Number----:"+i);
+        String url = "http://localhost:6603/getStudentName";
+        return restTemplate.getForObject(url , String.class);
+    }
+
+    public String getDefaultStudentName(Exception ex){
+        return "Default Ravikumar Lakkakula from CircuitBreker";
+    }
+}
---
+++ src/test/java/com/example/CircuitBreaker/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CircuitBreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
